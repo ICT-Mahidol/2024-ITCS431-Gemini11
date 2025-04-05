56b870a38d8f3c0a95b50df7d351fcca3d9a45be
Arus Thienmee <107537769+AruzCandlr@users.noreply.github.com>
2025-04-06 00:57:56 +0700

    Upload Gemini_login app
    If there's an error, it probably comes from the path at line2 in application.property. I set it as relative path, but if its not working, please use the absolute path from your device

---
+++ implementations/gemini_login/src/main/java/th/ac/mahidol/ict/gemini_login/GeminiLoginApplication.java
@@ -0,0 +1,12 @@
+package th.ac.mahidol.ict.gemini_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
+public class GeminiLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeminiLoginApplication.class, args);
+	}
+}
